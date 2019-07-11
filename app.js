9ef3e1e87020c1ce09c476797a4dd1dcf757510b
evan holt <adiga_knight@hotmail.com>
2019-07-11 13:19:32 +0300

    fixed port

--- app.js
+++ app.js
@@ -368,6 +368,6 @@ app.delete('files/:id',(req,res)=> {
   });
 });
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
